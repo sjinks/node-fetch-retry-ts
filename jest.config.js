7155e8eaeaabef94c77390771ddfd0c8c563c96d
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2020-11-01 04:41:57 +0200

    Update dependencies

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,11 @@ const merge = require('merge');
 const ts_preset = require('ts-jest/jest-preset');
 
 module.exports = merge.recursive(ts_preset, {
-    globals: {
-        'ts-jest': {
-            packageJson: 'package.json',
-        },
-    }
+    testPathIgnorePatterns: [
+        '<rootDir>/dist/',
+        '<rootDir>/node_modules/',
+    ],
+    collectCoverageFrom: [
+        '<rootDir>/src/*.ts',
+    ],
 });
